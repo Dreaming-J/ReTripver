45fedaa62486d8d32dd2ee7dd699c8e44fccdfea
Dreaming-J <kimjjong321@naver.com>
2024-11-07 14:45:20 +0900

    Feat: 여행지 정보 불러오기

--- src/main/java/com/retripver/plan/exception/ErrorCode.java
+++ src/main/java/com/retripver/plan/exception/ErrorCode.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 	
-	NOT_FOUND_PLAN(HttpStatus.INTERNAL_SERVER_ERROR.value(), "해당 여행 계획을 찾을 수 없습니다.");
+	NOT_FOUND_PLAN(HttpStatus.INTERNAL_SERVER_ERROR.value(), "해당 여행 계획을 찾을 수 없습니다."),
+	NOT_FOUND_ATTRACTION(HttpStatus.INTERNAL_SERVER_ERROR.value(), "해당 장소를 찾을 수 없습니다.");
 	
 	private Integer httpStatus;
 	private String message;
---
+++ src/main/java/com/retripver/plan/exception/NotFoundAttractionException.java
@@ -0,0 +1,5 @@
+package com.retripver.plan.exception;
+
+public class NotFoundAttractionException extends RuntimeException {
+
+}
