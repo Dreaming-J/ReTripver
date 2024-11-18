6729c27f9d6e8ce8acd31fb78ad48a620c599462
Dreaming-J <kimjjong321@naver.com>
2024-11-18 20:52:22 +0900

    Feat: 프론트 연동을 위한 CORS 설정

--- src/main/java/com/retripver/global/config/WebMvcConfig.java
+++ src/main/java/com/retripver/global/config/WebMvcConfig.java
@@ -1,17 +1,30 @@
 package com.retripver.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.retripver.user.interceptor.LoginInterceptor;
 
 @Configuration
+@EnableWebMvc
 class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoginInterceptor())
 				.addPathPatterns("/user/follow/**");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:5173")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+				.allowCredentials(true)
+				.exposedHeaders("*")
+				.maxAge(1800);
 	}
 }
