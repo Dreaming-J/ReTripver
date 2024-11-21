bfce4f1fd024e70f07c5c6b3998d05fad09780d6
정유진 <yueonq@gmail.com>
2024-11-21 17:52:26 +0900

    Feat: My Status 창 작업 중 - 레이아웃, 지도 hover 효과

--- src/views/user/StatusView.vue
+++ src/views/user/StatusView.vue
@@ -1,15 +1,37 @@
 <script setup>
-import StatusInfo from '@/components/user/status/StatusInfo.vue';
-import StatusMap from '@/components/user/status/StatusMap.vue';
+import StatusInfo from "@/components/user/status/StatusInfo.vue";
+import StatusMap from "@/components/user/status/StatusMap.vue";
+
+import { ref } from "vue";
+
+const isPanelOpen = ref(false);
+const selectedSido = ref(null);
+
+const open = () => {
+  isPanelOpen.vale = true;
+};
+
+const close = () => {
+  isPanelOpen.value = false;
+
+  setTimeout(() => {
+    selectedSido.vale = null;
+  }, 300);
+};
 </script>
 
 <template>
   <div class="status-container">
-    <div>
+    <div class="info-container">
       <StatusInfo />
     </div>
-    <div>
-      <StatusMap />
+    <div class="map-container">
+      <StatusMap @click="open" />
+    </div>
+    <div class="plans-container">
+      <div :is-open="isPanelOpen" @close="closePanel">
+        <div>여행 정보</div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,5 +39,22 @@ import StatusMap from '@/components/user/status/StatusMap.vue';
 <style scoped>
 .status-container {
   display: flex;
+  width: 100%;
+  height: 100%;
+}
+
+.info-container {
+  border: 1px solid pink;
+  width: 900px;
+}
+
+.map-container {
+  border: 1px solid red;
+  width: 100%;
+}
+
+.plans-container {
+  border: 1px solid black;
+  width: 900px;
 }
 </style>
